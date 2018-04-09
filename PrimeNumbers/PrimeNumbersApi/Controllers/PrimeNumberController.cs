d39442f0930488038acfec005dce6fea652c8fe6
savvas pamboukas <savvaspamboukas@hotmail.com>
2018-04-10 00:40:30 +0300

    create prime controller

---
+++ PrimeNumbers/PrimeNumbersApi/Controllers/PrimeNumberController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrimeNumbersApi.Controllers
+{
+    public class PrimeNumberController : Controller
+    {
+        [HttpGet("api/prime")]
+        public JsonResult GetPrimeNumber()
+        {
+            return new JsonResult(new List<object>()
+            {
+                new {id=0, PrimeNumbber=1}
+            });
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
